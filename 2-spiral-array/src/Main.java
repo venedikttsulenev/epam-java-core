0133d5b3715018dfa9880d4c70e21d25a012e92c
venedikttsulenev <venedikttsulenev@list.ru>
2018-02-17 23:26:12 +0300

    Task 2 refactored

--- 2-spiral-array/src/Main.java
+++ 2-spiral-array/src/Main.java
@@ -1,89 +1,94 @@
 public class Main {
-    static final byte UP = 0;
-    static final byte RIGHT = 1;
-    static final byte DOWN = 2;
-    static final byte LEFT = 3;
-    static final int I = 0;
-    static final int J = 1;
+    private enum Direction {
+        UP, RIGHT, DOWN, LEFT;
+
+        public Direction next() {
+            int nextOrd = (this.ordinal() + 1) % values().length;
+            return values()[nextOrd];
+        }
+    }
+
+    private static final int ROW = 0;
+    private static final int COLUMN = 1;
+
     /* Usage: java Main a00 a01 a02 ... */
     public static void main(String[] args) {
-        int [][] matrix = parseArgs(args);
+        int[][] matrix = parseArgs(args);
         if (null == matrix) {
             System.err.println("No matrix specified");
             return;
         }
         System.out.println("Matrix given:");
         printMatrix(matrix);
-        System.out.println();
 
+        System.out.println("Result:");
         int n = matrix.length;
-        byte dir = RIGHT;
-        int indices[] = new int[2];   // vector of indices: i == i[I], j == i[J]
+        Direction dir = Direction.RIGHT;
+        int indices[] = new int[]{0, 0};   // vector of indices: row number == i[ROW], column number == i[COLUMN]
         go(matrix, dir, indices, n);
-        dir = nextDir(dir);
-        while (n --> 0) {
+        dir = dir.next();
+        while (n-- > 0) {
             for (int k = 0; k < 2; ++k) {
                 go(matrix, dir, indices, n);
-                dir = nextDir(dir);
+                dir = dir.next();
             }
         }
         System.out.println();
     }
-    static void printMatrix(int m[][]) {
+
+    private static void printMatrix(int m[][]) {
         if (null == m)
             System.out.println("null");
         else {
-            for (int i = 0; i < m.length; ++i) {
-                if (null == m[i])
+            for (int[] row : m) {
+                if (null == row)
                     System.out.println("null");
                 else
-                    for (int j = 0; j < m[i].length; ++j)
-                        System.out.print(" " + m[i][j]);
+                    for (int element : row)
+                        System.out.print(" " + element);
                 System.out.println();
             }
         }
     }
-    static int [][] parseArgs(String [] args) {
+
+    private static int[][] parseArgs(String[] args) {
         if (args.length == 0)
             return null;
         int n = 1;
-        while (n*n < args.length)
+        while (n * n < args.length)
             ++n;
-        int [][] arr = new int[n][n];
+        int[][] arr = new int[n][n];
         for (int i = 0; i < args.length; ++i)
             arr[i / n][i % n] = Integer.parseInt(args[i]);
         return arr;
     }
-    static void go(int a[][], byte direction, int indices[], int howMuch) {
+
+    private static void go(int a[][], Direction direction, int indices[], int howMuch) {
         switch (direction) {
             case UP:
-                goI(a, -1, indices, I, howMuch);
-                ++indices[J];
+                goI(a, -1, indices, ROW, howMuch);
+                ++indices[COLUMN];
                 break;
             case DOWN:
-                goI(a, +1, indices, I, howMuch);
-                --indices[J];
+                goI(a, +1, indices, ROW, howMuch);
+                --indices[COLUMN];
                 break;
             case LEFT:
-                goI(a, -1, indices, J, howMuch);
-                --indices[I];
+                goI(a, -1, indices, COLUMN, howMuch);
+                --indices[ROW];
                 break;
             case RIGHT:
-                goI(a, +1, indices, J, howMuch);
-                ++indices[I];
+                goI(a, +1, indices, COLUMN, howMuch);
+                ++indices[ROW];
                 break;
         }
     }
-    static void goI(int a[][], int step, int i[], int indexNum, int howMuch) {
-        while (howMuch --> 0) {
-            System.out.print(" " + a[i[0]][i[1]]);
+
+    private static void goI(int a[][], int step, int i[], int indexNum, int howMuch) {
+        while (howMuch-- > 0) {
+            System.out.print(" " + a[i[ROW]][i[COLUMN]]);
             i[indexNum] += step;
         }
         i[indexNum] -= step;
-    }
-    static byte nextDir(byte dir) {
-        if (dir == 3)
-            return 0;
-        return ++dir;
     }
 }
